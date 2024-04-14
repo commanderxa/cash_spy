a910c99a984f1f3543fb5533f70dcc0fbd020922
Uesto228 <tima.ismailov.02@mail.ru>
2024-04-14 10:43:53 +0500

    jusan parser

--- parser/main.py
+++ parser/main.py
@@ -1,7 +1,9 @@
 import importlib.util
 import pathlib
+import requests
 
 DIR = "parsers"
+url = 'http://127.0.0.1:8000/api/v1/offers'
 
 for file in pathlib.Path(DIR).glob('*.py'):
     spec = importlib.util.spec_from_file_location(f"{__name__}.imported_{file.stem}", file)
@@ -10,3 +12,11 @@ for file in pathlib.Path(DIR).glob('*.py'):
 
     for name, category, card, partner, condition, cashback, fav in module.generate_offers():
         print(partner + ' ' + cashback)
+        data = {'name': name,
+                'category': category,
+                'card': card,
+                'partner': partner,
+                'condition': condition,
+                'cashback': cashback,
+                'fav': fav}
+        res = requests.post(url, data)
